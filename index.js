ccb73d724aa8eebaab676e52dfd1162ca776f322
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-06-13 16:58:21 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Penguin",
+    streetAddress: "north pole",
+}
+
+// function updateEmployeeWithKeyAndValue(obj, key, value) {
+//     let newNew = {...obj}
+//     return newNew.key = value 
+
+// }
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+      ...obj,
+      [key]: value,
+    };
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+         obj[key] = value 
+         return obj 
+  }
+
+  function deleteFromEmployeeByKey(obj, key) {
+   const newNew = {...obj,}
+    delete newNew[key]
+    return newNew
+  }
+
+  function  destructivelyDeleteFromEmployeeByKey(obj, key) {
+         delete obj[key]
+         return obj
+  }
